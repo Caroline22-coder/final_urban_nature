99ed32353515d6a5353bef0acf5a875f20414b0c
Caroline22-coder <caroline.cesselin@centrale-med.fr>
2025-11-18 16:46:50 +0100

    configure app name and icon

---
+++ app.config.js
@@ -0,0 +1,20 @@
+export default {
+  name: "UrbanNatureAR",
+  slug: "urban-nature-ar",
+  version: "1.0.0",
+  
+  // App icon
+  icon: "./assets/images/logo.png",
+  
+  ios: {
+    bundleIdentifier: "com.caroline.urbannaturear"
+  },
+  
+  android: {
+    package: "com.caroline.urbannaturear"
+  },
+  
+  experiments: {
+    typedRoutes: true
+  }
+};
